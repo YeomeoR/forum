260857ff1f14dc68490fbccd0936d34f83e9b564
YeomeoR <76974577+YeomeoR@users.noreply.github.com>
2021-01-31 15:38:30 +0000

    Fixed a memory issue

---
+++ actions/posts.js
@@ -0,0 +1,30 @@
+import * as api from '../api';
+
+// action creators (functions that return actions)
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+
+    dispatch({ type: 'FETCH_ALL', payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: 'CREATE', payload: data })
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-import express from 'express';
-import bodyparser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-// routes
-import postRoutes from './routes/posts.js'
+import reducers from './reducers'
 
-const app = express();
+import App from './App';
 
-//middleware
-
-app.use(bodyparser.json({ limit: '30mb', extended: true }));
-app.use(bodyparser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 
-app.use('/posts', postRoutes)
 
-//https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = 'mongodb+srv://YeomeoR:Cocoa3123@cluster0.gxh3a.mongodb.net/<dbname>?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`))).catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);;
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
